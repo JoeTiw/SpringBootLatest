01611041939221564b28143888e41fba88691538
bhupintiwari <bhupintiwari@gmail.com>
2021-11-30 22:28:29 -0600

    Day 2 of it..

--- src/main/java/com/example/springbootlatest/SpringBootLatestApplication.java
+++ src/main/java/com/example/springbootlatest/SpringBootLatestApplication.java
@@ -1,32 +1,15 @@
 package com.example.springbootlatest;
 
-import com.example.springbootlatest.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootLatestApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLatestApplication.class, args);
 
-    }
-    @GetMapping
-    public List<Customer> hello(){
-        return List.of(
-                new Customer(1L,
-                        "Joe",
-                        "joe@gmail.com",
-                        LocalDate.of(1997, 03, 25),
-                        21)
-        );
     }
 
 }
